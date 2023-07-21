a938b00635193076ef8ce39a63ab45e09921377d
Ratifcahyadi <ratifcahyadi@students.amikom.ac.id>
2023-07-21 14:20:08 +0700

    update logout profil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -252,7 +252,7 @@
                 <div class="modal-body">Pilih "Logout" di bawah jika ingin keluar.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Batal</button>
-                    <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a class="btn btn-primary" href="/logout">Logout</a>
                 </div>
             </div>
         </div>
